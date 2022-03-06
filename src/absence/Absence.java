46ae91c73dcb2b386f9eed404e2be2187f35fbd5
Matt RODRIGUEZ <78209324+MattRodriguez64@users.noreply.github.com>
2022-03-06 15:19:56 +0100

    Add files via upload

---
+++ src/absence/Absence.java
@@ -0,0 +1,71 @@
+package absence;
+
+import java.util.Date;
+
+import course.Course;
+import person.Student;
+
+
+public class Absence {
+
+	private Date absenceDate;
+	private String reason;
+	private Student student;
+	private Course course;
+	
+	public Absence() {}
+	
+	public Absence(Date absenceDate, String reason, Student student, Course course) {
+		setAbsenceDate(absenceDate);
+		setReason(reason);
+		setStudent(student);
+		setCourse(course);
+	}
+
+	public Date getAbsenceDate() {
+		return absenceDate;
+	}
+
+	public void setAbsenceDate(Date absenceDate) {
+		this.absenceDate = absenceDate;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+	
+	public String toString() {
+		String message;
+		message = "Absence : \nStudentID : "+getStudent().getStudentId()+"\nCourse : "+getCourse().getName()+"\nReason : "+getReason()+"\nAbsenceDate : "+getAbsenceDate();
+		return message;
+	}
+	
+	public boolean isEqual(Absence a) {
+		if((a.getStudent().getStudentId() == getStudent().getStudentId()) &&
+				(a.getReason().equals(getReason())) &&
+				(a.getCourse().getId() == getCourse().getId()) &&
+				(a.getAbsenceDate().equals(getAbsenceDate())))
+			return true;
+		return false;
+	}
+}
---
+++ src/registration/RegistrationManager.java
@@ -0,0 +1,86 @@
+package registration;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import csv.CsvFileManager;
+
+public class RegistrationManager implements CsvFileManager{
+	
+	private final String filepath = "D:\\Java-EclipseIDE\\eclipse-workspace\\java-scolarite\\src\\registration\\registrations.csv";
+	
+	private ArrayList<Registration> registrationList = new ArrayList<Registration>();
+	
+	public RegistrationManager() {}
+	
+	public RegistrationManager(ArrayList<Registration> registrationList) {
+		setRegistrationList(registrationList);
+	}
+	
+	public ArrayList<Registration> getRegistrationList() {
+		return registrationList;
+	}
+
+	public void setRegistrationList(ArrayList<Registration> registrationList) {
+		this.registrationList = registrationList;
+	}
+
+	@Override
+	public void writeCsvFile() {
+		try {
+			BufferedWriter buffer = new BufferedWriter(new FileWriter(filepath));
+			if(!getRegistrationList().isEmpty()) {
+				for(Registration registration : getRegistrationList())
+					buffer.append(registration.getStudent().getStudentId()+","+registration.getCourse().getId()+"\n");
+				buffer.flush();
+				buffer.close();
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void readCsvFile(Object[] params) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public Registration getRegistration(int studentId, int courseId) {
+		Registration currentRegistration = new Registration();
+		if(!getRegistrationList().isEmpty()) {
+			for(Registration registration : getRegistrationList()) {
+				if((registration.getStudent().getStudentId() == studentId) && (registration.getCourse().getId() == courseId)) {
+					currentRegistration = registration;
+					break;
+				}
+			}
+		}
+		return currentRegistration;
+	}
+	
+	public void addRegistration(Registration registration) {
+		boolean isUnique = true;
+		for(Registration currentRegistration : getRegistrationList()) {
+			if(currentRegistration.isEqual(registration)) {
+				isUnique = false;
+				break;
+			}
+		}
+		if(isUnique) {
+			getRegistrationList().add(registration);
+			writeCsvFile();
+		}
+	}
+	
+	public void deleteRegistration(Registration registration) {
+		getRegistrationList().remove(registration);
+		writeCsvFile();
+	}
+
+
+}
